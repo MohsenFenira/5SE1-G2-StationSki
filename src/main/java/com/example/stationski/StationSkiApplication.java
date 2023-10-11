d13801174fd93e0d014bc3174686dd5ba23b6211
naderjardak <naderjardak5@gmail.com>
2023-10-11 17:37:44 +0100

    init

---
+++ src/main/java/com/example/stationski/StationSkiApplication.java
@@ -0,0 +1,17 @@
+package com.example.stationski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAspectJAutoProxy
+public class StationSkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StationSkiApplication.class, args);
+    }
+
+}
